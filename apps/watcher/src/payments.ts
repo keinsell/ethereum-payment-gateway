1a97ce70809e4e56dfb654495d059026e133b6a3
Jakub Olan <keinsell@protonmail.com>
2022-08-12 22:27:54 +0200

    docs: :bulb: add `PaymentStatus` documentation

--- apps/watcher/src/payments.ts
+++ apps/watcher/src/payments.ts
@@ -11,15 +11,29 @@ import schedule from "node-schedule";
 import ms from "ms";
 
 export enum PaymentStatus {
+  /** Default transaction status once they are created in system. */
   initalized = "INITALIZED",
+  /** Status of Paymment when it's added to our balance listener which will watch for payments. */
   waitingForPayment = "WAITING_FOR_PAYMENT",
+  /** Status of {@link IPayment payment} when user will deposit too little, such transactions will wait until expiration date if deposit will be not satisfied to declared amount system should send refund transaction. */
   underpaid = "UNDERPAID",
+  /**
+   * Status of {@link IPayment payment} which happens when end-customer deposited too much capital than was previously declared by system. Such transaction will result in sending declared amount and refunding additional funds deposited by user.
+   */
   overpaid = "OVERPAID",
+  /**
+   * Status of {@link IPayment payment} when we catch unconfirmed balance on our wallets, this step will result in {@link PaymentStatus.pendingConfirmation `PENDING_CONFIRMATION`} status as we should wait for at least one confirmation.
+   */
   paymentRecived = "PAYMENT_RECIVED",
-  pendingConfirmation = "PENDING_CONFIRMALATION",
+  pendingConfirmation = "PENDING_CONFIRMATION",
   confirmed = "CONFIRMED",
+  /** Performing internal actions to complete transaction such as accouting goods which should be acciured by performing declared transaction. */
+  delivery = "DELIVERY",
+  /** Status where we returned deposited funds to end-customer as declared amount could not be satified and we could not take actions. */
   refunded = "REFUNDED",
+  expired = "EXPIRED",
   completed = "COMPLETED",
+  completedWithRefund = "COMPLETED_WITH_REFUND",
 }
 
 export interface IPayment {
