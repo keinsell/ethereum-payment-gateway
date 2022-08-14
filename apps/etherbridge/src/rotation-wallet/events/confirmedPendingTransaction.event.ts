f66733564fcad8a5c7d9f61cbb8e0f823c89ffe9
Jakub Olan <keinsell@protonmail.com>
2022-08-14 13:29:04 +0200

    refactor: ♻️ attach `Transaction` to `ConfirmedPendingTransactionEvent`

--- apps/etherbridge/src/rotation-wallet/events/confirmedPendingTransaction.event.ts
+++ apps/etherbridge/src/rotation-wallet/events/confirmedPendingTransaction.event.ts
@@ -2,14 +2,13 @@ import { DomesticEvent } from "../../infra/event"
 import consola from 'consola'
 import { ConsoleStyle } from "../../utilities/console-colors.util"
 import { Transaction } from "web3-eth"
-import { IRotationWallet } from "../rotation-wallet.repository"
 
 export class ConfirmedPendingTransactionEvent extends DomesticEvent {
-    constructor(transactionHash: string) {
-      super("pending-transaction-found", transactionHash);
+    constructor(transactionHash: string, transaction?: Transaction) {
+      super("pending-transaction-found", {transactionHash: transactionHash, transaction: transaction});
     }
     override toConsole(): void {
-        const message = `Confirmed pending transaction ${ConsoleStyle.identifier(this.data)}`
+        const message = `Confirmed pending transaction ${ConsoleStyle.identifier(this.data.transactionHash)}`
 
         consola.success(message)
     }
