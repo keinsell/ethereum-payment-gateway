fb7ad102ee36d79bb59f2ecad8f6070143cecabf
Jakub Olan <keinsell@protonmail.com>
2022-08-14 06:34:55 +0200

    docs: 💡 add documentation to `PaymentStatus`

--- apps/watcher/src/payment/payment.repository.ts
+++ apps/watcher/src/payment/payment.repository.ts
@@ -4,6 +4,17 @@ import { nanoid } from "nanoid";
 import random from "random-js";
 import { IRotationWallet } from "../rotation-wallet/rotation-wallet.repository";
 
+/**
+ * Payment Status stands for globally defined statuses that are used to define current state of payment declaration.
+ *
+ * 1. {@link PaymentStatus.initalized **INITALIZED**}, payment declaration was created but it's not ready in our system yet to recive transaction.
+ * 2. {@link PaymentStatus.waitingForPayment **WAITING_FOR_PAYMENT**}, payment declaration will start to watch rotation wallet for payments, from now it's ready to be paid.
+ * 3. {@link PaymentStatus.pendingConfirmation **PENDING_CONFIRMATION**}, payment declaration seen payment on rotation wallet but will wait for at least one confirmation before proceeding.
+ * 4. {@link PaymentStatus.paymentRecived **PAYMENT_RECIVED**}, payment declaration confirmed deposit on rotation wallet. This can result in other status when conditions for **PAYMENT_RECIVED** will be not satified. For example: if end-customer will pay more than declared amount there we'll apply {@link PaymentStatus.overpaid **OVERPAID**} status.
+ * 5. {@link PaymentStatus.overpaid **OVERPAID**} & {@link PaymentStatus.underpaid **UNDERPAID**} situational statuses which will be applied when user will mistake amount sent to payment declaration. System will automatically try to resolve such issues, in case of **UNDERPAID** system will wait for additional payment from end-customer and if such will be not met until expiration time, transaction will be **REFUNDED**. If **OVERPAID** system will refund additional payment to end-customer by provided refund address, to reduce transaction size to previously set.
+ * 6. {@link PaymentStatus.delivery **DELIVERY**}, payment declaration is performing deliver to end-customer.
+ * 7. {@link PaymentStatus.completed **COMPLETED**}, {@link PaymentStatus.refunded **REFUNDED**} and {@link PaymentStatus.expired **EXPIRED**}. Several statues that end payment delclaration just for different reasons.
+ */
 export enum PaymentStatus {
   /** Default transaction status once they are created in system. */
   initalized = "INITALIZED",
