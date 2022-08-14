518e85341183d2fa1babb2df6010c5033e77a314
Jakub Olan <keinsell@protonmail.com>
2022-08-14 07:10:02 +0200

    chore: 🔨 add `ganache` development blockchain

--- apps/watcher/src/config/environment.ts
+++ apps/watcher/src/config/environment.ts
@@ -1,2 +1,2 @@
-export const WEBSOCKET_PROVIDER_URL = "ws://localhost:7545";
-export const RPC_PROVIDER_URL = "http://localhost:7545";
+export const WEBSOCKET_PROVIDER_URL = "ws://localhost:8545";
+export const RPC_PROVIDER_URL = "http://localhost:8545";
