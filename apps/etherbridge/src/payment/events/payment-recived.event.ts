ff46c3af85301e55558a00544aeab7d6e23440f9
Jakub Olan <keinsell@protonmail.com>
2022-08-18 00:22:45 +0200

    refactor: 🔊 add `PaymentRecivedEvent`

---
+++ apps/etherbridge/src/payment/events/payment-recived.event.ts
@@ -0,0 +1,21 @@
+import { DomesticEvent } from "../../infra/event"
+import consola from 'consola'
+import { ConsoleStyle } from "../../utilities/console-colors.util"
+import { Transaction } from "web3-eth"
+
+export class PaymentRecivedEvent extends DomesticEvent {
+    constructor(payment: any) {
+      super("payment-recived", {
+        payment
+      });
+    }
+
+    override toConsole(): void {
+        const paymentIdentificator = `${ConsoleStyle.identifier(this.data.payment.id)}`
+
+        const message = `Recived payment for purchase ${paymentIdentificator}`
+
+        consola.success(message)
+    }
+  }
+  
