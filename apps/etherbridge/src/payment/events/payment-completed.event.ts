d5fd162d270fea944113d649c2927eaabd3a4310
Jakub Olan <keinsell@protonmail.com>
2022-08-18 00:23:09 +0200

    refactor: 🔊 add `PaymentCompletedEvent`

---
+++ apps/etherbridge/src/payment/events/payment-completed.event.ts
@@ -0,0 +1,19 @@
+import { DomesticEvent } from "../../infra/event"
+import consola from 'consola'
+import { ConsoleStyle } from "../../utilities/console-colors.util"
+import { Transaction } from "web3-eth"
+
+export class PaymentCompletedEvent extends DomesticEvent {
+    constructor(payment: any) {
+      super("payment-completed", {
+        payment
+      });
+    }
+
+    override toConsole(): void {
+        const message = `Completed payment ${ConsoleStyle.identifier(this.data.payment.id)}`
+
+        consola.success(message)
+    }
+  }
+  
