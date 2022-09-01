9f98ffdc4557780932129f1101f1857a7f7d9df7
Jakub Olan <keinsell@protonmail.com>
2022-09-01 23:49:47 +0200

    fix: :bug: organise exports under `blockchain.module`

--- apps/etherbridge/src/modules/blockchain/blockchain.module.ts
+++ apps/etherbridge/src/modules/blockchain/blockchain.module.ts
@@ -2,12 +2,12 @@ import { GANACHE_BLOCKCHAIN_CONFIG } from "../../config/blockchain.config";
 import { EthereumNetworkService } from "./services/networks/ethereum/ethereum.service";
 
 export namespace BlockchainModule {
-  export const service = services.ganache;
-
   export namespace services {
     export const ganache = new EthereumNetworkService(
       "ganache",
       GANACHE_BLOCKCHAIN_CONFIG
     );
   }
+
+  export const service = services.ganache;
 }
