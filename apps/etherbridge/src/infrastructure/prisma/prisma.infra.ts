65150a1ace3d12c3c307af676e1f706680135c00
Jakub Olan <keinsell@protonmail.com>
2022-09-03 03:19:58 +0200

    docs: :memo: add TODO comment for #33

--- apps/etherbridge/src/infrastructure/prisma/prisma.infra.ts
+++ apps/etherbridge/src/infrastructure/prisma/prisma.infra.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "prisma-internal";
 
+// TODO(#33): When database is turned off on system startup, software will throw an error and ocassionally crash. - https://github.com/keinsell/etherbridge/issues/33
 const PrismaConnectionInstance = new PrismaClient();
 await PrismaConnectionInstance.$connect();
 
